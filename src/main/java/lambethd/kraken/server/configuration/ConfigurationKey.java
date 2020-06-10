183ef337c94aeb83032a6a75eae7a4d782b5df23
David Lambeth <dl561@bath.ac.uk>
2020-06-10 13:49:42 +0100

    commiting

--- src/main/java/lambethd/kraken/server/configuration/ConfigurationKey.java
+++
@@ -1,6 +0,0 @@
-package lambethd.kraken.server.configuration;
-
-public class ConfigurationKey {
-    public static final String ALPHA_VANTAGE_URL_KEY = "ALPHA_VANTAGE_URL";
-    public static final String ALPHA_VANTAGE_API_KEY_KEY = "ALPHA_VANTAGE_API_KEY";
-}
--- src/main/java/lambethd/kraken/server/configuration/RepositorySetup.java
+++ src/main/java/lambethd/kraken/server/configuration/RepositorySetup.java
@@ -1,17 +1,11 @@
 package lambethd.kraken.server.configuration;
 
 
-import lambethd.kraken.data.mongo.repository.IConfigRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RepositorySetup {
 
-    @Autowired
-    private IConfigRepository configRepository;
-
-    public void setup(){
-        configRepository.deleteAll();
+    public void setup() {
     }
 }
